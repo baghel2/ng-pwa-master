006c9592de99a4550493ffa1761b6d372583f400
arv <arvindsinghbaghel700@gmail.com>
2018-08-29 17:54:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
 btnAdd.addEventListener('click', (e) => {
   // hide our user interface that shows our A2HS button
   btnAdd.style.display = 'none';
-  alert('hiii');
   // Show the prompt
   deferredPrompt.prompt();
   // Wait for the user to respond to the prompt
